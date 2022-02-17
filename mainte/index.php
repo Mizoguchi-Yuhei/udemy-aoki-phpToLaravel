da519fae6ef71525f76d3c1bc447ac0573706899
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2022-02-17 22:52:05 +0900

    セクション2 52

---
+++ mainte/index.php
@@ -0,0 +1,31 @@
+<?php
+
+// ファイルの読み込み
+require 'db_connection.php';
+
+// ユーザー入力なし query
+// $sql = "select * from contacts where id = 2";
+// $stmt = $pdo->query($sql);  // sql実行 ステートメント
+
+// $result = $stmt->fetchAll();
+
+// echo '<pre>';
+// var_dump($result);
+// echo '</pre>';
+
+
+// ユーザー入力あり prepare bind execute SQLインジェクション対策
+// $sql = "select * from contacts where id = ?";  // プレイスホルダー
+$sql = "select * from contacts where id = :id";  // 名前付きプレイスホルダー
+$stmt = $pdo->prepare($sql);  // プリペアードステイトメント
+$stmt->bindValue('id', 3, PDO::PARAM_INT);  // 紐付け
+$stmt->execute();  // 実行
+
+$result2 = $stmt->fetchAll();
+
+echo '<pre>';
+var_dump($result2);
+echo '</pre>';
+
+
+// トランザクション まとまって処理
